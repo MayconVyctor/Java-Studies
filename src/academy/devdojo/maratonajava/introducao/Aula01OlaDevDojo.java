1203b1487d528563a53cededca2a15146b013c34
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2025-07-23 16:06:55 -0300

    First Commit of Studies of Java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentario <b>javadoc</b>
+ */
+public class Aula01OlaDevDojo {
+    //Isso é um comentario de uma linha
+    /*
+        isso é um comentario de multiplas linhas
+    */
+
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAA");
+    }
+}
